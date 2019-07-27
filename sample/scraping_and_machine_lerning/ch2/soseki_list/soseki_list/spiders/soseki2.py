479162b6d7497968dd829aba48984176b354a48e
oratta <oratta@gmail.com>
2019-07-28 07:52:26 +0900

    scrapyのサンプル2
    リンクのリストをjsonファイルとして出力

---
+++ sample/scraping_and_machine_lerning/ch2/soseki_list/soseki_list/spiders/soseki2.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class Soseki2Spider(scrapy.Spider):
+    name = 'soseki2'
+    start_urls = [
+        'https://www.aozora.gr.jp/index_pages/person148.html'
+    ]
+
+    def parse(selfself, response):
+        li_list = response.css('ol > li a')
+        for a in li_list:
+            href = a.css('::attr(href)').extract_first()
+            text = a.css('::text').extract_first()
+            href2 = response.urljoin(href)
+            yield {
+                'text': text,
+                'url': href2
+            }
