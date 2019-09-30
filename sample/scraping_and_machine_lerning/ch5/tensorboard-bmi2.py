f614cdd1be2e970260a8fe6c311a2f2522fc467d
oratta <oratta@gmail.com>
2019-09-30 20:31:18 +0900

    TensorBoardでの表示用の調整

--- sample/scraping_and_machine_lerning/ch5/tensorboard-bmi2.py
+++ sample/scraping_and_machine_lerning/ch5/tensorboard-bmi2.py
@@ -19,42 +19,47 @@ test_ans = list(test_csv["label_pat"])
 
 # データフローグラフを構築する --- (*5)
 # データを入れるプレースホルダを宣言
-x  = tf.placeholder(tf.float32, [None, 2]) # 身長,体重のデータを入れる
-y_ = tf.placeholder(tf.float32, [None, 3]) # 答えのラベルを入れる
+x  = tf.placeholder(tf.float32, [None, 2], name="x")
+y_ = tf.placeholder(tf.float32, [None, 3], name="y_")
 
 # 変数を宣言 --- (*6)
-W = tf.Variable(tf.zeros([2, 3])); # 重み
-b = tf.Variable(tf.zeros([3])); # バイアス
-# ソフトマックス回帰を定義 --- (*7)
-y = tf.nn.softmax(tf.matmul(x, W) + b)
+with tf.name_scope('interface') as scope:
+    W = tf.Variable(tf.zeros([2, 3]), name="W"); # 重み
+    b = tf.Variable(tf.zeros([3]), name="b"); # バイアス
+    # ソフトマックス回帰を定義 --- (*7)
+    with tf.name_scope('softmax') as scope:
+        y = tf.nn.softmax(tf.matmul(x, W) + b)
 
 # モデルを訓練する --- (*8)
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-optimizer = tf.train.GradientDescentOptimizer(0.01)
-train = optimizer.minimize(cross_entropy)
+with tf.name_scope('loss') as scope:
+    cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+
+with tf.name_scope('training') as scope:
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = optimizer.minimize(cross_entropy)
 
 # 正解率を求める
-predict = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(predict, tf.float32))
+with tf.name_scope('accuracy') as scope:
+    predict = tf.equal(tf.argmax(y, 1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(predict, tf.float32))
 
 # セッションを開始
-sess = tf.Session()
-tw = tf.summary.FileWriter("log_dir", graph=sess.graph)
+with tf.Session() as sess:
+    tw = tf.summary.FileWriter("log_dir", graph=sess.graph)
+    sess.run(tf.global_variables_initializer()) #変数を初期化
+    # テストデータを用いて学習させる
+    for step in range(3500):
+        i = (step * 100) % 14000
+        rows = csv[1 + i : 1 + i + 100]
+        x_pat = rows[["weight","height"]]
+        y_ans = list(rows["label_pat"])
+        fd = {x: x_pat, y_: y_ans}
+        sess.run(train, feed_dict=fd)
+        if step % 500 == 0:
+            cre = sess.run(cross_entropy, feed_dict=fd)
+            acc = sess.run(accuracy, feed_dict={x: test_pat, y_: test_ans})
+            print("step=", step, "cre=", cre, "acc=", acc)
 
-sess.run(tf.initialize_all_variables()) #変数を初期化
-# テストデータを用いて学習させる
-for step in range(3500):
-    i = (step * 100) % 14000
-    rows = csv[1 + i : 1 + i + 100]
-    x_pat = rows[["weight","height"]]
-    y_ans = list(rows["label_pat"])
-    fd = {x: x_pat, y_: y_ans}
-    sess.run(train, feed_dict=fd)
-    if step % 500 == 0:
-        cre = sess.run(cross_entropy, feed_dict=fd)
-        acc = sess.run(accuracy, feed_dict={x: test_pat, y_: test_ans})
-        print("step=", step, "cre=", cre, "acc=", acc)
-
-# 最終的な正解率を求める
-acc = sess.run(accuracy, feed_dict={x: test_pat, y_: test_ans})
-print("正解率=", acc)
+    # 最終的な正解率を求める
+    acc = sess.run(accuracy, feed_dict={x: test_pat, y_: test_ans})
+    print("正解率=", acc)
