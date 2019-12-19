b115e4ad1c7a3e48c9b4fcb1cf8789de4c65c983
oratta <oratta@gmail.com>
2019-12-20 08:10:22 +0900

    [wip]MNIT neuralnetwork

---
+++ sample/oreilly_deep_learning/ch3/mnist_util.py
@@ -0,0 +1,13 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import load_mnist
+from PIL import Image
+
+def img_show(mnist_img):
+    pil_img = Image.formarray(np.unit8(mnist_img))
+    pil_img.show()
+
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
