d4d3ec9233e9a9f98f1056b50b80e6e9358b817f
oratta <oratta@gmail.com>
2019-12-22 09:16:30 +0900

    operation check mnist

--- sample/oreilly_deep_learning/ch3/mnist_util.py
+++ sample/oreilly_deep_learning/ch3/mnist_util.py
@@ -5,7 +5,7 @@ from dataset.mnist import load_mnist
 from PIL import Image
 
 def img_show(mnist_img):
-    pil_img = Image.formarray(np.unit8(mnist_img))
+    pil_img = Image.fromarray(np.uint8(mnist_img))
     pil_img.show()
 
 def get_data():
