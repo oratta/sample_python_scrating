787c48595cae8a4b17b83ca156b61429108e6429
oratta <oratta@gmail.com>
2019-12-22 11:44:54 +0900

    [sample]ニューラルネットワーク実装

--- sample/oreilly_deep_learning/ch3/mnist_util.py
+++ sample/oreilly_deep_learning/ch3/mnist_util.py
@@ -1,8 +1,9 @@
-import sys, os
+import sys, os, pickle
 sys.path.append(os.pardir)
 import numpy as np
 from dataset.mnist import load_mnist
 from PIL import Image
+from common.functions import sigmoid, softmax
 
 def img_show(mnist_img):
     pil_img = Image.fromarray(np.uint8(mnist_img))
@@ -11,3 +12,22 @@ def img_show(mnist_img):
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
     return x_test, t_test
+
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+
+    return network
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
